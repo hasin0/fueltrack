1402f2e956c7677d88775ca7a28b1bdcfaf025f8
hasin0 <hasino2258@gmail.com>
2022-07-20 12:41:31 +0100

    done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Auth::routes(['register'=>false]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -32,6 +35,6 @@ Route::group(['prefix'=>'admin','middleware'=>'auth'],function(){
 
 
 
-//require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';
 
 
